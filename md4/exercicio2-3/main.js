8f8e6876ac4c4cd15a9aaf2b97127615d58a8b9d
FernandesMN <fernandes.matheuscp@gmail.com>
2020-01-23 09:35:07 -0300

    ending exercise 3

--- md4/exercicio2-3/main.js
+++ md4/exercicio2-3/main.js
@@ -1,21 +1,44 @@
 var buttonElement = document.querySelector('.btn');
 var containerElement = document.querySelector('.repositorys');
 
+function renderLoading() {
+    containerElement.innerHTML = "";
+    var loadElement = document.createElement('li')
+    var textLoad = document.createTextNode('Carregando...')
+
+    loadElement.appendChild(textLoad);
+    containerElement.appendChild(loadElement);
+}
+
+function renderErro() {
+    containerElement.innerHTML = "";
+    var errorElement = document.createElement('li')
+    var textError = document.createTextNode('Erro')
+
+    errorElement.appendChild(textError);
+    containerElement.appendChild(errorElement);
+}
+
 buttonElement.onclick = function renderRepository() {
     function repository() {
         return new Promise((resolve, reject) => {
             var valueInput = document.querySelector('input[name=user]');
+            var user = valueInput.value;
+
+            if (!user) return;
+            renderLoading();
+
             var xhr = new XMLHttpRequest();
             xhr.open('GET','https://api.github.com/users/' + valueInput.value + '/repos');
             xhr.send(null);
     
             xhr.onreadystatechange = function() {
-               if (xhr.readyState === 4) {
+                if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
                         resolve(JSON.parse(xhr.responseText));
                     }
                     else {
-                        reject('Error 404');
+                        reject();
                     }
                 } 
             }
@@ -24,6 +47,7 @@ buttonElement.onclick = function renderRepository() {
     
     repository()
         .then(function(response) {
+            containerElement.innerHTML = "";
             for(repos of response) {
                 var liElement = document.createElement('li');
                 var linkElement = document.createElement('a');
@@ -35,7 +59,7 @@ buttonElement.onclick = function renderRepository() {
                 containerElement.appendChild(liElement);
             }
         })
-        .catch(function(error) {
-            console.log(error);
+        .catch(function() {
+            renderErro();
         })
 };
